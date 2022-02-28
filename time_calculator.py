d6e229fc789f8f4081786d1c53a77cc650115504
Jessica Olsen <jaolsen@Jessicas-MacBook-Pro.local>
2022-02-28 09:11:11 -0500

    Added conversion to 24-hr day and Day count

--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,35 @@
-def add_time(start, duration):
+def add_time(start, duration, day = False):
+    start_time = start.split()
+    beg_time = start_time[0]
+    beg_hours = beg_time.split(':')[0]
+    beg_minutes = beg_time.split(':')[1]
+    am_pm = start_time[1]
+    
+    add_hours = duration.split(':')[0]
+    add_minutes = duration.split(':')[1]
+    day_count = 0
+    
+    #converting to a 24 hour period
+    if am_pm == "PM" : 
+        beg_hours = int(beg_hours) + 12
+    
+    #Adding time
+    new_hours = int(beg_hours) + int(add_hours)
+    new_minutes = int(beg_minutes) + int(add_minutes)
+
+    #Determing Day count
+    if new_hours > 24 : 
+        num_days = new_hours / 24
+        day_count = num_days
+
+    #Determining which day it is
+    
+    return day_count
+
+    
 
 
+    #return new_time
 
 
-
-    return new_time
+print(add_time("11:06 PM", "2:02", 'TueSday'))
